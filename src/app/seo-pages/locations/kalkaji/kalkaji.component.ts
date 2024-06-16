98e673206cd4c59c604e6ccff3653c0c91a9df37
Himanshu3198 <abtonpower.sales@gmail.com>
2024-06-17 01:43:25 +0530

    feat-seo-pages-kalkaji

---
+++ src/app/seo-pages/locations/kalkaji/kalkaji.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './kalkaji.component.html',
+  styleUrls: ['./kalkaji.component.css','./kalkaji.component.css']
+})
+export class KalkajiComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
